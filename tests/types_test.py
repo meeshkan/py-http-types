90167a9501dcab27192e6da5f4459b22c25f6363
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-12-10 13:23:18 +0200

    Add tests and refactor.

---
+++ tests/types_test.py
@@ -0,0 +1,22 @@
+from http_types import Request, Response
+from typeguard import check_type
+
+req = Request(method="get",
+              host="api.github.com",
+              path="/user/repos",
+              pathname="/user/repos",
+              protocol="https",
+              query={},
+              body="",
+              body_as_json="",
+              headers={})
+
+res = Response(status_code=200, body="OK", body_as_json="OK")
+
+
+def test_request_typechecks():
+    check_type("req", req, Request)
+
+
+def test_response_typechecks():
+    check_type("res", res, Response)
